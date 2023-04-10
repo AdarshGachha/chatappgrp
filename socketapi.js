e06e3d56b937df985194db51b94d4ed9aa5a0cb4
iammradarsh <gachhaadarsh122@gmail.com>
2023-04-11 00:17:46 +0530

    first commit

---
+++ socketapi.js
@@ -0,0 +1,40 @@
+
+
+const io = require( "socket.io" )();
+const socketapi = {
+    io: io
+};
+
+
+var onlineusernames = [];
+var onlineuserids = [];
+
+// Add your socket.io logic here!
+io.on( "connection", function( socket ) {
+
+    socket.on("nameset",function(data){
+        onlineuserids.push(socket.id);
+        onlineusernames.push(data);
+    io.emit("online",onlineusernames);
+
+    })
+
+    
+
+    console.log( "A user connected" );
+    socket.on("msg",function(data){
+        io.emit("msg",data)
+    })
+
+    socket.on("typing",function(){
+        socket.broadcast.emit("typing");
+    })
+    socket.on("disconnect",function(){
+        onlineusernames.splice(onlineuserids.indexOf(socket.id),1);
+        onlineuserids.splice(onlineuserids.indexOf(socket.id),1);
+        io.emit("online",onlineusernames);
+    })
+});
+// end of socket.io logic
+
+module.exports = socketapi;
